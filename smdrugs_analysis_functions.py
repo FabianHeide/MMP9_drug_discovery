4d63b12cbfff27f787da0bd5df8313645269c41e
Fabian Heide <84485398+FabianHeide@users.noreply.github.com>
2023-03-02 22:53:54 -0600

    Add files via upload

--- smdrugs_analysis_functions.py
+++ smdrugs_analysis_functions.py
@@ -6,7 +6,7 @@ This set of functions is used to generate and process drug-like parameter featur
 import pandas as pd
 import numpy as np
 from rdkit import Chem
-from rdkit.Chem import Descriptors, Lipinski
+from rdkit.Chem import Descriptors, Lipinski, rdMolDescriptors
 
 #function that calculates lipinski values based on a SMILES line code
 def lipinski(smiles, verbose=False):
@@ -21,15 +21,17 @@ def lipinski(smiles, verbose=False):
         i=0  
         for mol in molecule_data:        
 
-            paramater_MolWt = Descriptors.MolWt(mol)
-            paramater_MolLogP = Descriptors.MolLogP(mol)
-            paramater_NumHDonors = Lipinski.NumHDonors(mol)
-            paramater_NumHAcceptors = Lipinski.NumHAcceptors(mol)
+            parameter_MolWt = Descriptors.MolWt(mol)
+            parameter_MolLogP = Descriptors.MolLogP(mol)
+            parameter_NumHDonors = Lipinski.NumHDonors(mol)
+            parameter_NumHAcceptors = Lipinski.NumHAcceptors(mol)
+            parameter_TPSA = rdMolDescriptors.CalcTPSA(mol)
 
-            row = np.array([paramater_MolWt,
-                            paramater_MolLogP,
-                            paramater_NumHDonors,
-                            paramater_NumHAcceptors])   
+            row = np.array([parameter_MolWt,
+                            parameter_MolLogP,
+                            parameter_NumHDonors,
+                            parameter_NumHAcceptors,
+                            parameter_TPSA])   
 
             if(i==0):
                 baseData=row
@@ -37,13 +39,14 @@ def lipinski(smiles, verbose=False):
                 baseData=np.vstack([baseData, row])
             i=i+1      
 
-        columnNames=["MW","LogP","NumHDonors","NumHAcceptors"]   
+        columnNames=['MW','LogP','NumHDonors','NumHAcceptors', 'TPSA']   
         parameters = pd.DataFrame(data=baseData,columns=columnNames)
         
     except:
-        print('Check for NaN values in the SMILES column')
+        print('Check for NaN values in the smiles column')
 
     return parameters
+
 
 
 #normalize IC50 values to 100000000 or below
@@ -73,3 +76,45 @@ def pIC50(input):
     x = input.drop('standard_value_norm', 1)
         
     return x
+
+
+#function that calculates Mann-Whitney statistics and then puts out a summary table
+def mannwhitney(descriptor, verbose=False): 
+    from numpy.random import seed
+    from numpy.random import randn
+    from scipy.stats import mannwhitneyu
+
+    # seed the random number generator
+    seed(1)
+
+    # actives and inactives
+    selection = [descriptor, 'activity_class']
+    df = df1[selection]
+    active = df[df['activity_class'] == 'active']
+    active = active[descriptor]
+
+    selection = [descriptor, 'activity_class']
+    df = df1[selection]
+    inactive = df[df['activity_class'] == 'inactive']
+    inactive = inactive[descriptor]
+
+    # compare samples
+    stat, p = mannwhitneyu(active, inactive)
+    #print('Statistics=%.3f, p=%.3f' % (stat, p))
+    
+    # interpret
+    alpha = 0.05
+    if p > alpha:
+        interpretation = 'Same distribution (fail to reject H0)'
+    else:
+        interpretation = 'Different distribution (reject H0)'
+    
+    results = pd.DataFrame({'Parameter':descriptor,
+                            'Statistics':stat,
+                            'p':p,
+                            'alpha':alpha,
+                            'Interpretation':interpretation}, index=[0])
+    filename = 'mannwhitneyu_' + descriptor + '.csv'
+    results.to_csv(f'data/{filename}', index=False)
+
+    return results
